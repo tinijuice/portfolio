c4aa22027e0b24a8fee7badc562d1b64b2d39d64
Léo Philippe <leophilippe14@gmail.com>
2026-01-17 23:11:04 +0100

    add

--- src/main.js
+++ src/main.js
@@ -37,11 +37,12 @@ async function setProjects() {
 
         template.querySelector('.thumbnail img').src = project.thumbnail
 
-        template.querySelector('a').href = project.link
+        template.querySelector('.thumbnail a').href = project.link
+        template.querySelector('.title a').href = project.link
 
         template.querySelector('.project').style = '--index: ' + (Number(index) + 1)
 
-        
+
         fragment.append(template)
     });
 
@@ -49,3 +50,122 @@ async function setProjects() {
 }
 
 setProjects()
+
+
+
+function glitchedText(element, maxStep = 3, delay = 100, duration = 150) {
+
+    const letters = "abcdefghijklmnopqrstuvwxyz";
+
+    const original = element.dataset.text;
+    if (!original) return;
+
+    const chars = original.split('');
+
+    element.textContent = chars.map(() => letters[Math.floor(Math.random() * letters.length)]).join('')
+
+    chars.forEach((letter, index) => {
+
+        let step = 0;
+
+        setTimeout(() => {
+
+            const interval = setInterval(() => {
+
+                chars[index] = letters[Math.floor(Math.random() * letters.length)];
+
+                element.textContent = chars.join('');
+
+                if (step >= maxStep) {
+                    clearInterval(interval)
+                    chars[index] = letter
+                    element.textContent = chars.join('')
+                }
+
+                step++
+
+            }, duration);
+        }, index * delay);
+
+
+
+    });
+
+}
+
+setInterval(() => {
+
+    document.querySelectorAll('.glitched-text').forEach(el => glitchedText(el, 5, 50, 100));
+}, 6000);
+
+
+function callback(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            glitchedText(entry.target, 5, 50, 150);
+            entry.target.classList.add('isView');
+            observer.unobserve(entry.target); t
+        }
+    });
+}
+
+const observer = new IntersectionObserver(callback, {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.5
+});
+
+document.querySelectorAll(".glitched-text:not(.isView)").forEach(el => {
+    observer.observe(el);
+});
+
+
+
+
+
+
+
+// function decryptWordSequential(element, changes = 3, intervalTime = 100, delayBetween = 150) {
+//     const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+//     const original = element.textContent;
+//     if (!original) return;
+
+//     const chars = original.split(""); // lettres finales
+
+//     // On initialise le mot en crypté
+//     element.textContent = chars.map(() => letters[Math.floor(Math.random() * letters.length)]).join("");
+
+//     // On anime chaque lettre une par une
+//     chars.forEach((finalChar, index) => {
+//         let step = 0;
+
+//         setTimeout(() => {
+//             const interval = setInterval(() => {
+//                 if (step >= changes) {
+//                     clearInterval(interval);
+//                     // on met la vraie lettre finale
+//                     element.textContent = chars.map((c, i) => i <= index ? c : element.textContent[i]).join("");
+//                     return;
+//                 }
+
+//                 // lettre en cours devient aléatoire, les autres restent
+//                 element.textContent = chars.map((c, i) => {
+//                     if (i === index) return letters[Math.floor(Math.random() * letters.length)];
+//                     return element.textContent[i]; // les lettres précédentes restent fixées
+//                 }).join("");
+
+//                 step++;
+//             }, intervalTime);
+//         }, index * delayBetween);
+//     });
+// }
+
+
+
+// const title = document.querySelector(".glitched-text");
+
+// setTimeout(() => {
+//     decryptWordSequential(title, 5, 50, 50);
+// }, 1000);
+
+
--- src/projects.js
+++ src/projects.js
@@ -8,5 +8,21 @@ export const projects = [
         "technology": "Javascript & PHP",
         "thumbnail": "https://cdn.prod.website-files.com/68fe16075ad6bebbd10ad31c/6909609fae58867aae3b3934_CleanShot%202025-11-03%20at%2020.47.02%402x-p-1080.png",
         "link": ""
+    },
+    {
+        "title": "Rave",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quidem mollitia fugiat quod tempore adipisci, nam nihil consequuntur consequatur repudiandae possimus. Laborum fuga magnam tempore nobis fugiat quo maiores aperiam.",
+        "type": "Developpement Web",
+        "technology": "Javascript & PHP",
+        "thumbnail": "https://cdn.prod.website-files.com/68fe16075ad6bebbd10ad31c/6909609fae58867aae3b3934_CleanShot%202025-11-03%20at%2020.47.02%402x-p-1080.png",
+        "link": ""
+    },
+    {
+        "title": "Rave",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quidem mollitia fugiat quod tempore adipisci, nam nihil consequuntur consequatur repudiandae possimus. Laborum fuga magnam tempore nobis fugiat quo maiores aperiam.",
+        "type": "Developpement Web",
+        "technology": "Javascript & PHP",
+        "thumbnail": "https://cdn.prod.website-files.com/68fe16075ad6bebbd10ad31c/6909609fae58867aae3b3934_CleanShot%202025-11-03%20at%2020.47.02%402x-p-1080.png",
+        "link": ""
     }
 ]
