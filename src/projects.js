ff8009bdce43911a64d480ea7352a1c369c6001d
Léo Philippe <leophilippe14@gmail.com>
2026-01-20 13:07:09 +0100

    add

--- src/projects.js
+++ src/projects.js
@@ -6,7 +6,23 @@ export const projects = [
         "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quidem mollitia fugiat quod tempore adipisci, nam nihil consequuntur consequatur repudiandae possimus. Laborum fuga magnam tempore nobis fugiat quo maiores aperiam.",
         "type": "Developpement Web",
         "technology": "Javascript & PHP",
-        "thumbnail": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/960px-Placeholder_view_vector.svg.png",
+        "thumbnail": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQA-7UIYLcEK1mAwct1XgW8aSMnvm3ZEQBYgQ&s",
         "link": ""
-    }
+    },
+    {
+        "title": "Rave",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quidem mollitia fugiat quod tempore adipisci, nam nihil consequuntur consequatur repudiandae possimus. Laborum fuga magnam tempore nobis fugiat quo maiores aperiam.",
+        "type": "Developpement Web",
+        "technology": "Javascript & PHP",
+        "thumbnail": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQA-7UIYLcEK1mAwct1XgW8aSMnvm3ZEQBYgQ&s",
+        "link": ""
+    },
+    {
+        "title": "Rave",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quidem mollitia fugiat quod tempore adipisci, nam nihil consequuntur consequatur repudiandae possimus. Laborum fuga magnam tempore nobis fugiat quo maiores aperiam.",
+        "type": "Developpement Web",
+        "technology": "Javascript & PHP",
+        "thumbnail": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQA-7UIYLcEK1mAwct1XgW8aSMnvm3ZEQBYgQ&s",
+        "link": ""
+    },
 ]
