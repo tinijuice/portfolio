ce980f029aa0c8b83cb4f372fe5e350eff54707b
Léo Philippe <leophilippe14@gmail.com>
2026-01-22 14:05:20 +0100

    add

--- src/projects.js
+++ src/projects.js
@@ -6,7 +6,7 @@ export const projects = [
         "description": "Refonte complète du site Rave, incluant le design et le développement. Mise en place d’un système de filtres et de favoris afin d’améliorer l’expérience utilisateur et la navigation.",
         "type": "Maquettage & développement web",
         "technology": "PHP & SQL",
-        "thumbnail": "/portfolio/src/assets/img/rave-thumbnail.jpg",
+        "thumbnail": "assets/img/rave-thumbnail.jpg",
         "link": "/portfolio/rave"
     },
     {
@@ -14,7 +14,7 @@ export const projects = [
         "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quidem mollitia fugiat quod tempore adipisci, nam nihil consequuntur consequatur repudiandae possimus. Laborum fuga magnam tempore nobis fugiat quo maiores aperiam.",
         "type": "XXX",
         "technology": "XXX & XXX",
-        "thumbnail": "/portfolio/src/assets/img/placeholder.png",
+        "thumbnail": "assets/img/placeholder.png",
         "link": ""
     },
     {
